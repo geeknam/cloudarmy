f903971cad6d8f37b9c94addc2fa741e01fd83af
Nam Ngo <emoinrp@gmail.com>
2016-03-08 17:52:55 +1100

    Add some explanation.

---
+++ examples/simple_instance/__init__.py
@@ -0,0 +1,16 @@
+
+
+"""
+
+This example spins up an EC2 instance:
+
+cloudarmy ~/cloudarmy/examples/simple_instance staging
+
+--> t1.micro EC2 instance, outputs PublicIP
+
+cloudarmy ~/cloudarmy/examples/simple_instance production
+
+--> t2.medium EC2 instance, does not output PublicIP
+
+
+"""
